75aa942894043eb6b8f6584ceeff12de2c55468a
Iain <tomlinsoniain@gmail.com>
2023-03-04 08:29:05 -0800

    Login
    New form and route for logging users in

--- routes.py
+++ routes.py
@@ -1,7 +1,17 @@
-from flask import app, render_template
+from flask import app, redirect, render_template, flash
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
